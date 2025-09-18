72fdc33a50b8269d50a6c13eee9d41f77bf9c314
Student Levi Rosheuvel <lrosheuv6991@student.landstede.nl>
2025-09-18 13:41:35 +0200

    ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  // ssr: true,
+  ssr: true,
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   css: ["~/assets/css/main.css", "leaflet/dist/leaflet.css"],
