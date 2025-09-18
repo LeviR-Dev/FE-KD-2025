071b7b4da538e8d040141e25d999e0f2bdbf0e09
Student Levi Rosheuvel <lrosheuv6991@student.landstede.nl>
2025-09-18 14:09:04 +0200

    seo

--- app/app.vue
+++ app/app.vue
@@ -6,21 +6,13 @@
   </UApp>
   
 </template>
-<script lang="ts">
+<script setup lang="ts">
 import { useSeoMeta } from '#app'
-
-export default {
-  head: {
-    meta: [
-      { name: 'keywords', content: 'jungle tuin, tuien, jungle, jungle tuin aanleggen' }
-    ]
-  }
-}
 
 useSeoMeta({
   title: 'Jungle Tuinen',
   description: 'Deze website is gericht om mensen te inspireren om hun eigen jungle tuin te maken. Met ons stappenplan kun je eenvoudig een weelderige jungle realiseren.',
-  // keywords: 'jungle tuin, tuinen, jungle, jungle tuin aanleggen',
+  keywords: 'jungle tuin, tuinen, jungle, jungle tuin aanleggen',
   ogTitle: 'Jungle Tuinen',
   ogDescription: 'Deze website is gericht om mensen te inspireren om hun eigen jungle tuin te maken. Met ons stappenplan kun je eenvoudig een weelderige jungle realiseren.',
   ogImage: 'https://frontend-kd-levi.reachservers.nl/_nuxt/JungleBannerLarge.D9Cmv5Lc.svg',
