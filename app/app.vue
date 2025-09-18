eea5fb1ad24d8b8506c07d80434b3c7f5730aafb
Student Levi Rosheuvel <lrosheuv6991@student.landstede.nl>
2025-09-18 14:10:35 +0200

    seo

--- app/app.vue
+++ app/app.vue
@@ -4,19 +4,23 @@
       <NuxtPage />
     </NuxtLayout>
   </UApp>
-  
 </template>
-<script setup lang="ts">
-import { useSeoMeta } from '#app'
 
-useSeoMeta({
-  title: 'Jungle Tuinen',
-  description: 'Deze website is gericht om mensen te inspireren om hun eigen jungle tuin te maken. Met ons stappenplan kun je eenvoudig een weelderige jungle realiseren.',
-  keywords: 'jungle tuin, tuinen, jungle, jungle tuin aanleggen',
-  ogTitle: 'Jungle Tuinen',
-  ogDescription: 'Deze website is gericht om mensen te inspireren om hun eigen jungle tuin te maken. Met ons stappenplan kun je eenvoudig een weelderige jungle realiseren.',
-  ogImage: 'https://frontend-kd-levi.reachservers.nl/_nuxt/JungleBannerLarge.D9Cmv5Lc.svg',
-  ogUrl: 'https://frontend-kd-levi.reachservers.nl',
-  ogType: 'website',
-})
+<script lang="ts">
+export default {
+  head() {
+    return {
+      title: 'Jungle Tuinen',
+      meta: [
+        { name: 'description', content: 'Deze website is gericht om mensen te inspireren om hun eigen jungle tuin te maken. Met ons stappenplan kun je eenvoudig een weelderige jungle realiseren.' },
+        { name: 'keywords', content: 'jungle tuin, tuinen, jungle, jungle tuin aanleggen' },
+        { property: 'og:title', content: 'Jungle Tuinen' },
+        { property: 'og:description', content: 'Deze website is gericht om mensen te inspireren om hun eigen jungle tuin te maken. Met ons stappenplan kun je eenvoudig een weelderige jungle realiseren.' },
+        { property: 'og:image', content: 'https://frontend-kd-levi.reachservers.nl/_nuxt/JungleBannerLarge.D9Cmv5Lc.svg' },
+        { property: 'og:url', content: 'https://frontend-kd-levi.reachservers.nl' },
+        { property: 'og:type', content: 'website' },
+      ]
+    }
+  }
+}
 </script>
