cd4535e07e97dedc7a173e3a5d7d3e6502afa3f9
Student Levi Rosheuvel <lrosheuv6991@student.landstede.nl>
2025-09-22 10:58:17 +0200

    removed config head bc it overrides the useSeoMeta in the wrong way

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,21 +10,4 @@ export default defineNuxtConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  app: {
-    head: {
-      title: "Jungle Tuinen – Inspiratie & Tips voor een Groene Oase",
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1, maximum-scale=5",
-      meta: [
-        {
-          name: "description",
-          content:
-            "Ontdek alles over jungle tuinen: tips, inspiratie en onderhoud. Bezoek Jungletuinen.nl voor unieke ideeën en maak van jouw tuin een tropisch paradijs",
-        },
-      ],
-      htmlAttrs: {
-        lang: "nl",
-      },
-    },
-  },
 });
